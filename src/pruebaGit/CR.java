27051338ddf217ced59025b60d79b9797d6a622a
Carolina Rodríguez <carolinarb1994@gmail.com>
2023-06-05 12:23:17 +0200

    Modificación 1

--- src/pruebaGit/CR.java
+++ src/pruebaGit/CR.java
@@ -3,7 +3,9 @@ package pruebaGit;
 public class CR {
 
 	public static void main(String[] args) {
+		
 	System.out.println("Alumno: Carolina Rodríguez");
+	System.out.println("Modificación desdeMaster");
 		
 	}
 
